f59cbf106abdd14cd81f3271fa8673453fba2415
cardandres <65802352+cardandres@users.noreply.github.com>
2022-12-07 14:36:08 -0500

    Access article title and description

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ api_key = '5803574c55b04535837cc22cefbcc1ce'
 url = 'https://newsapi.org/v2/everything?q=tesla&from=2022-11-07&\
     sortBy=publishedAt&apiKey=5803574c55b04535837cc22cefbcc1ce'
 
+# Make request
 request = requests.get(url)
-content = request.text
 
+# Get a dictionary with data
+content = request.json()
 
-
-print(content)
+# Access the article titles and description
+for article in content['articles']:
+    print(article['title'])
+    print(article['description'])
